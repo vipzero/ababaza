1f29bac48e4d32e940594a2d6c92849e08376802
vipzero <hiroflighter@gmail.com>
2021-01-21 11:32:44 +0900

    add snapshot test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+
 import App from './App'
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
 	const div = document.createElement('div')
+
 	ReactDOM.render(<App />, div)
 	ReactDOM.unmountComponentAtNode(div)
+
+	const tree = renderer.create(<App />).toJSON()
+
+	expect(tree).toMatchSnapshot()
 })
